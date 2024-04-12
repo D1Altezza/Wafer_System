54261920c2f675bda52f8f2b79637a25e66539b4
User <User@LAPTOP-V3B8II23>
2024-04-12 09:16:28 +0800

    no message

--- Main.cs
+++ Main.cs
@@ -33,7 +33,7 @@ namespace Wafer_System
 {
     public partial class Main : Form
     {
-        public bool pass = true;
+        public bool pass = false;
         LogRW logRW = new LogRW();
         ConfigWR configWR;
         public LiteDatabase db;
